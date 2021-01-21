c9a845bb762a814f3439875abbcc179bb2b78173
Skliznyak <77763198+Skliznyak@users.noreply.github.com>
2021-01-21 04:29:19 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,22 @@
+'use strict';
+
+
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer(function (request, response) {
+    console.log(request.method, request.url);
+
+    if (request.url == '/style.css')
+    {
+        const text = fs.readFileSync('style.css', 'utf8');
+        response.end(text);
+    }
+    if (request.url == '/') {
+        const text = fs.readFileSync('index.html', 'utf8');
+        response.end(text);
+    }
+
+});
+server.listen(3000)
+console.log('server started');
